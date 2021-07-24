c30df83beccae76653c111475464fd2f67d2a3c9
Casey Johnson <johnsoncm3@gmail.com>
2021-07-24 10:23:24 -0400

    committing files

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,6 +1,8 @@
 const { AuthenticationError } = require('apollo-server-express');
 const { User, Product, Category, Order } = require('../models');
 const { signToken } = require('../utils/auth');
+
+//HIDE
 const stripe = require('stripe')('sk_test_4eC39HqLyjWDarjtT1zdp7dc');
 
 const resolvers = {
