024e0bccc9b5ad1041426b9a5a820f04f3a4c0ae
Catherine Smith <catsmith24601@gmail.com>
2021-07-24 10:29:37 -0400

    fixed exporting typo in index.js in models

--- server/models/index.js
+++ server/models/index.js
@@ -1,6 +1,6 @@
 const User = require('./User');
 const Event = require('./Event');
 const Category = require('./Category');
-const Order = require('./Order');
+const List = require('./List');
 
-module.exports = { User, Event, Category, Order };
+module.exports = { User, Event, Category, List };
