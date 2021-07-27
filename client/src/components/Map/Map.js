9607bb40dc86e7869ec485c6233c87997831ee26
Seth <sethvance26@gmail.com>
2021-07-27 14:37:58 -0400

    adding /map route with a basic map centered on RVA

--- client/src/components/Map/Map.js
+++ client/src/components/Map/Map.js
@@ -4,11 +4,11 @@ import ReactMapGL from 'react-map-gl';
 
 function Map() {
   const [viewport, setViewport] = useState({
-    width: 400,
-    height: 400,
-    latitude: 37.7577,
-    longitude: -122.4376,
-    zoom: 8
+    width: 700,
+    height: 700,
+    latitude: 37.541290,
+    longitude: -77.434769,
+    zoom: 12
   });
 
   return (
